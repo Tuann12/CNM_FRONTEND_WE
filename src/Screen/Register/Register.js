47ef073b4ceed3bb54a3cca3c3abbcdf86fba6f1
Nguyen Thanh Tuan <ngthtuan333@gmail.com>
2024-03-02 19:57:11 +0700

    Fixing code warning

--- src/Screen/Register/Register.js
+++ src/Screen/Register/Register.js
@@ -2,46 +2,51 @@ import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLock, faMobileAlt, faUser } from "@fortawesome/free-solid-svg-icons";
 import styles from "./Register.module.scss";
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import { yupResolver } from '@hookform/resolvers/yup';
+import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
 const Register = () => {
-//   const history = useHistory();
-//   const dispatch = useDispatch();
+  //   const history = useHistory();
+  //   const dispatch = useDispatch();
   const schema = yup.object().shape({
-    phone: yup.string().trim().matches(/^(?:\d{10}|(84|0[3|5|7|8|9])+([0-9]{8})\b|\w+@\w+\.\w{2,3})$/, 'Số điện thoại hoặc email không hợp lệ').required(),
-    password: yup.string().min(8, 'Mật khẩu phải trên 8 kí tự').required(),
+    phone: yup
+      .string()
+      .trim()
+      .matches(
+        /^(?:\d{10}|(84|0[3|5|7|8|9])+([0-9]{8})\b|\w+@\w+\.\w{2,3})$/,
+        "Số điện thoại hoặc email không hợp lệ"
+      )
+      .required(),
+    password: yup.string().min(8, "Mật khẩu phải trên 8 kí tự").required(),
   });
   const {
     register,
-    handleSubmit,
-    reset,
     formState: { errors },
   } = useForm({
-    resolver: yupResolver(schema)
+    resolver: yupResolver(schema),
   });
 
   const [pass, setPass] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [repeatPass, setRepeatPass] = useState("");
 
-//   const user = useSelector((state) => state.user);
-//   const { error } = user;
+  //   const user = useSelector((state) => state.user);
+  //   const { error } = user;
 
-//   const onSubmit = async (data) => {
-//     if (pass === repeatPass) {
-//       await dispatch(registerUserRequest(data, () => {
-//         history.push('/login');
-//       }));
-//     } else {
-//       setErrorMessage('Mật khẩu không khớp');
-//       setTimeout(() => {
-//         setErrorMessage('');
-//       }, 2000);
-//     }
-//   };
+  //   const onSubmit = async (data) => {
+  //     if (pass === repeatPass) {
+  //       await dispatch(registerUserRequest(data, () => {
+  //         history.push('/login');
+  //       }));
+  //     } else {
+  //       setErrorMessage('Mật khẩu không khớp');
+  //       setTimeout(() => {
+  //         setErrorMessage('');
+  //       }, 2000);
+  //     }
+  //   };
 
   return (
     <div className={styles.container}>
@@ -70,9 +75,11 @@ const Register = () => {
             <span>
               <FontAwesomeIcon icon={faMobileAlt}></FontAwesomeIcon>
             </span>
-            {
-              errors.phone ? (<div className={styles.error}>{errors.phone?.message}</div>) : ''
-            }
+            {errors.phone ? (
+              <div className={styles.error}>{errors.phone?.message}</div>
+            ) : (
+              ""
+            )}
             {/* {error ? <div className={styles.error}>{error}</div> : ""} */}
           </div>
           <div className={styles.register_form_input}>
@@ -81,14 +88,14 @@ const Register = () => {
               placeholder="Mật khẩu"
               required
               {...register("password")}
-              onChange={(e) =>
-                setPass(e.target.value)
-              }
+              onChange={(e) => setPass(e.target.value)}
             ></input>
             <div className={styles.error}>{errors.password?.message}</div>
-            {
-              errors.password ? (<div className={styles.error}>{errors.password?.message}</div>) : ''
-            }
+            {errors.password ? (
+              <div className={styles.error}>{errors.password?.message}</div>
+            ) : (
+              ""
+            )}
             <span>
               <FontAwesomeIcon icon={faLock}></FontAwesomeIcon>
             </span>
@@ -98,16 +105,16 @@ const Register = () => {
               type="password"
               placeholder="Nhập lại mật khẩu"
               required
-              onChange={(e) =>
-                setRepeatPass(e.target.value)
-              }
+              onChange={(e) => setRepeatPass(e.target.value)}
             ></input>
             <span>
               <FontAwesomeIcon icon={faLock}></FontAwesomeIcon>
             </span>
-            {
-              errorMessage ? (<div className={styles.error}>{errorMessage}</div>) : ''
-            }
+            {errorMessage ? (
+              <div className={styles.error}>{errorMessage}</div>
+            ) : (
+              ""
+            )}
           </div>
 
           <button className={styles.btn}>Đăng kí</button>
