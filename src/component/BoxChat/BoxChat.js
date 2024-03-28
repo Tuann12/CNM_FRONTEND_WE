100694be2f8bb1b151631ebb9190241603f3918c
Nguyen Thanh Tuan <ngthtuan333@gmail.com>
2024-03-28 22:29:34 +0700

    move background without app.js

--- src/component/BoxChat/BoxChat.js
+++ src/component/BoxChat/BoxChat.js
@@ -3,7 +3,6 @@ import ListChat from './ListChat/ListChat';
 import ItemChat from './ListChat/ItemChat';
 import styles from './BoxChat.module.scss';
 import classNames from 'classnames/bind';
-import avt1 from '../../images/image1.jpeg';
 
 const cx = classNames.bind(styles);
 
@@ -18,7 +17,13 @@ function BoxChat() {
         <div>
             <ListChat>
                 <ItemChat
-                    avatar={<img className={cx('avatarImg')} src={avt1} alt="avatar" />}
+                    avatar={
+                        <img
+                            className={cx('avatarImg')}
+                            src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vietnamworks.com%2Fhrinsider%2Fanh-dai-dien-facebook-y-nghia.html&psig=AOvVaw3OCxQGJVjRnjJJ6yo4eA0J&ust=1711724887601000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLjqqpiel4UDFQAAAAAdAAAAABAK"
+                            alt="avatar"
+                        />
+                    }
                     title="Nguyen Van A"
                     contentChat="Hello"
                     time="14 Minutes"
--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -2,7 +2,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComment, faContactBook, faGear } from '@fortawesome/free-solid-svg-icons';
 import classNames from 'classnames/bind';
 import styles from './Navbar.module.scss';
-import avt1 from '../../images/image1.jpeg';
 
 const cx = classNames.bind(styles);
 
@@ -12,7 +11,11 @@ function Navbar() {
             <div className={cx('container')}>
                 <div className={cx('topIcon')}>
                     <div className={cx('avatar')}>
-                        <img className={cx('avatarImg')} src={avt1} alt="avatar" />
+                        <img
+                            className={cx('avatarImg')}
+                            src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vietnamworks.com%2Fhrinsider%2Fanh-dai-dien-facebook-y-nghia.html&psig=AOvVaw3OCxQGJVjRnjJJ6yo4eA0J&ust=1711724887601000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLjqqpiel4UDFQAAAAAdAAAAABAK"
+                            alt="avatar"
+                        />
                     </div>
                     <div className={cx('wrapIcon')}>
                         <div className={cx('boxIcon', 'active')}>
