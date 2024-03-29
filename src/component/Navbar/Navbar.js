b822db523c280fe0cba2b45af7c7c063d6046c7c
Nguyen Thanh Tuan <ngthtuan333@gmail.com>
2024-03-29 21:39:49 +0700

    Refactor Navbar component and add ShowInfo component

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,22 +1,62 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComment, faContactBook, faGear } from '@fortawesome/free-solid-svg-icons';
+import ShowInfo from '../ShowInfo/ShowInfo';
+import React, { useState } from 'react';
+import Tippy from '@tippyjs/react/headless';
 import classNames from 'classnames/bind';
 import styles from './Navbar.module.scss';
 
 const cx = classNames.bind(styles);
 
 function Navbar() {
+    const [showTippy, setShowTippy] = useState(false);
+    const [showInfo, setShowInfo] = useState(false);
+
+    const handleTippyVisibility = () => {
+        setShowTippy(!showTippy);
+        setShowInfo(false);
+    };
+
+    const handleProfileClick = () => {
+        setShowTippy(false);
+        setShowInfo(true);
+    };
+
     return (
         <div className={cx('wrapper')}>
             <div className={cx('container')}>
                 <div className={cx('topIcon')}>
-                    <div className={cx('avatar')}>
-                        <img
-                            className={cx('avatarImg')}
-                            src="https://nhadepso.com/wp-content/uploads/2023/03/loa-mat-voi-101-hinh-anh-avatar-meo-cute-dang-yeu-dep-mat_2.jpg"
-                            alt="avatar"
-                        />
-                    </div>
+                    <Tippy
+                        interactive
+                        placement="right"
+                        trigger="click"
+                        visible={showTippy}
+                        onClickOutside={() => setShowTippy(false)}
+                        render={(attrs) => (
+                            <div className={cx('profile')} tabIndex="-1" {...attrs}>
+                                <div className={cx('wrapProfile')}>
+                                    <div className={cx('header')}>Tuấn Nguyễn</div>
+                                    <div className={cx('content')}>
+                                        <div className={cx('info')} onClick={handleProfileClick}>
+                                            Hồ sơ của bạn
+                                        </div>
+                                        {/* </Tippy> */}
+                                        <div className={cx('setting')}>Cài đặt</div>
+                                    </div>
+                                    <div className={cx('logout')}>Đăng xuất</div>
+                                </div>
+                            </div>
+                        )}
+                    >
+                        <div className={cx('avatar')} onClick={handleTippyVisibility}>
+                            <img
+                                className={cx('avatarImg')}
+                                src="https://nhadepso.com/wp-content/uploads/2023/03/loa-mat-voi-101-hinh-anh-avatar-meo-cute-dang-yeu-dep-mat_2.jpg"
+                                alt="avatar"
+                            />
+                        </div>
+                    </Tippy>
+                    {showInfo && <ShowInfo onHide={handleTippyVisibility} />}
                     <div className={cx('wrapIcon')}>
                         <div className={cx('boxIcon', 'active')}>
                             <FontAwesomeIcon className={cx('icon')} icon={faComment} />
---
+++ src/component/ShowInfo/ShowInfo.js
@@ -0,0 +1,78 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState, useEffect } from 'react';
+import { faXmark, faPenToSquare, faCamera } from '@fortawesome/free-solid-svg-icons';
+import classNames from 'classnames/bind';
+import styles from './ShowInfo.module.scss';
+
+const cx = classNames.bind(styles);
+
+function ShowInfo({ onHide }) {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        setIsVisible(true);
+    }, []);
+
+    const handleHide = () => {
+        onHide();
+    };
+
+    return (
+        <div>
+            <div className={cx('overlay')}></div>
+            <div className={cx('wrapper', { visible: isVisible })}>
+                <div className={cx('profileHeader')}>
+                    <h3 className={cx('title')}>Thông tin tài khoản</h3>
+                    <div className={cx('boxIcon')} onClick={handleHide}>
+                        <FontAwesomeIcon className={cx('icon')} icon={faXmark} />
+                    </div>
+                </div>
+                <div className={cx('profileContent')}>
+                    <div className={cx('banner')}>
+                        <img
+                            className={cx('bannerImg')}
+                            src="https://cover-talk.zadn.vn/5/a/1/b/7/dc602e48929270e32806de153cb3c2e9.jpg"
+                            alt="banner"
+                        />
+                    </div>
+                    <div className={cx('boxAvt')}>
+                        <div className={cx('avt')}>
+                            <img
+                                className={cx('avtImg')}
+                                src="https://zpsocial-f52-org.zadn.vn/435e11d21a79fb27a268.jpg"
+                                alt="avt"
+                            />
+                            <div className={cx('editAvt')}>
+                                <FontAwesomeIcon className={cx('iconEditAvt')} icon={faCamera} />
+                            </div>
+                        </div>
+                        <div className={cx('boxTitle')}>
+                            <h3 className={cx('title')}> Tuấn Nguyễn </h3>
+                            <div className={cx('titleIcon')}>
+                                <FontAwesomeIcon className={cx('icon')} icon={faPenToSquare} />
+                            </div>
+                        </div>
+                    </div>
+                    <div className={cx('info')}>
+                        <h3 className={cx('infoTitle')}>Thông tin cá nhân</h3>
+                        <div className={cx('infoSex')}>
+                            <span className={cx('sex')}> Giới tính </span>
+                            <span className={cx('sexResult')}> Nam</span>
+                        </div>
+                        <div className={cx('infoEmail')}>
+                            <span className={cx('email')}> Email</span>
+                            <span className={cx('emailResult')}> ngthtuan333@gmail.com</span>
+                        </div>
+                        <div className={cx('update')}>
+                            <div className={cx('updateContainer')}>
+                                <FontAwesomeIcon className={cx('updateIcon')} icon={faPenToSquare} />
+                                <h3 className={cx('updateTitle')}>Cập nhật</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default ShowInfo;
