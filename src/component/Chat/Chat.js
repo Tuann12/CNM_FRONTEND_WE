4f4f2f075cf7663fae7ff89f9ea69cb89e299387
Nguyen Thanh Tuan <ngthtuan333@gmail.com>
2024-04-04 20:29:36 +0700

    fix warning

--- src/component/Chat/Chat.js
+++ src/component/Chat/Chat.js
@@ -10,9 +10,9 @@ const cx = classNames.bind(styles);
 function Chat() {
     const [selectedItem, setSelectedItem] = useState({ avatar: '', title: '' });
 
-    const handleItemClick = ({ avatar, title }) => {
-        setSelectedItem({ avatar, title });
-    };
+    // const handleItemClick = ({ avatar, title }) => {
+    //     setSelectedItem({ avatar, title });
+    // };
 
     return (
         <div className={cx('wrapper')}>
