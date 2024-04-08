bf44bc532b7a6c9e350f6d35a82ba0c99c916990
MinhAn <voongocminhan20072002@gmail.com>
2024-04-08 16:18:31 +0700

    get friends send request to uset

--- src/Screen/Contact/Contact.js
+++ src/Screen/Contact/Contact.js
@@ -1,81 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck, faXmark } from '@fortawesome/free-solid-svg-icons';
-import styles from './Contact.module.scss';
 import classNames from 'classnames/bind';
 import ListChat from '../../component/BoxChat/ListChat/ListChat';
 import ItemChat from '../../component/BoxChat/ListChat/ItemChat';
 import Navbar from '../../component/Navbar/Navbar';
+import axios from 'axios';
+import styles from './Contact.module.scss';
 
 const cx = classNames.bind(styles);
 
 function Contact() {
+    const [friendRequestsSent, setFriendRequestsSent] = useState([]);
+    const storedData = localStorage.getItem('loginData');
+
+    useEffect(() => {
+        const fetchFriendRequestsSent = async () => {
+            try {
+                const userId = JSON.parse(storedData).foundUser._id; // Replace 'userId' with the actual user ID
+                const response = await axios.get(`http://localhost:4000/user/getFriendRequestsSentToUser/${userId}`);
+                setFriendRequestsSent(response.data.friendRequestsSent);
+            } catch (error) {
+                console.error('Error fetching friend requests sent:', error);
+            }
+        };
+
+        fetchFriendRequestsSent();
+    }, []);
+
     return (
         <div className={cx('wrapper')}>
             <Navbar />
             <div className={cx('container')}>
                 <ListChat>
-                    <div className={cx('itemChat')}>
-                        <ItemChat
-                            avatar={
-                                <img
-                                    className={cx('avatarImg')}
-                                    src="https://nhadepso.com/wp-content/uploads/2023/03/loa-mat-voi-101-hinh-anh-avatar-meo-cute-dang-yeu-dep-mat_2.jpg"
-                                    alt="avatar"
-                                />
-                            }
-                            title="Nguyen Van A"
-                            email="ngthtuan333@gmail.com"
-                            iconAccept={faCheck}
-                            iconDecline={faXmark}
-                        />
-                    </div>
-                    <div className={cx('itemChat')}>
-                        <ItemChat
-                            avatar={
-                                <img
-                                    className={cx('avatarImg')}
-                                    src="https://nhadepso.com/wp-content/uploads/2023/03/loa-mat-voi-101-hinh-anh-avatar-meo-cute-dang-yeu-dep-mat_2.jpg"
-                                    alt="avatar"
-                                />
-                            }
-                            title="Nguyen Van A"
-                            email="ngthtuan333@gmail.com"
-                            iconAccept={faCheck}
-                            iconDecline={faXmark}
-                        />
-                    </div>{' '}
-                    <div className={cx('itemChat')}>
-                        <ItemChat
-                            avatar={
-                                <img
-                                    className={cx('avatarImg')}
-                                    src="https://nhadepso.com/wp-content/uploads/2023/03/loa-mat-voi-101-hinh-anh-avatar-meo-cute-dang-yeu-dep-mat_2.jpg"
-                                    alt="avatar"
-                                />
-                            }
-                            title="Nguyen Van A"
-                            email="ngthtuan333@gmail.com"
-                            iconAccept={faCheck}
-                            iconDecline={faXmark}
-                        />
-                    </div>{' '}
-                    <div className={cx('itemChat')}>
-                        <ItemChat
-                            avatar={
-                                <img
-                                    className={cx('avatarImg')}
-                                    src="https://nhadepso.com/wp-content/uploads/2023/03/loa-mat-voi-101-hinh-anh-avatar-meo-cute-dang-yeu-dep-mat_2.jpg"
-                                    alt="avatar"
-                                />
-                            }
-                            title="Nguyen Van A"
-                            email="ngthtuan333@gmail.com"
-                            iconAccept={faCheck}
-                            iconDecline={faXmark}
-                        />
-                    </div>
+                    {friendRequestsSent.map((user, index) => (
+                        <div key={index} className={cx('itemChat')}>
+                            <ItemChat
+                                avatar={<img className={cx('avatarImg')} src={user.avatar} alt="avatar" />}
+                                title={user.name}
+                                email={user.email}
+                                iconAccept={faCheck}
+                                iconDecline={faXmark}
+                            />
+                        </div>
+                    ))}
                 </ListChat>
             </div>
         </div>
     );
 }
+
 export default Contact;
